e2f7e24e0823f678d7e5c064ba51340543a2cc5b
Norma Zambrano <normazambrano@Normas-MBP.attlocal.net>
2019-01-05 13:01:12 -0800

    first commit

---
+++ Concentration/Controller/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Angel Zambrano on 1/5/19.
+//  Copyright © 2019 Icon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func touchCard(_ sender: UIButton) {
+     // implement this later
+    }
+    
+}
+
